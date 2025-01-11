2c5da03790874cb0dd0045dafdd0679248236a82
Jere Mäennenä <jere.maennena@gmail.com>
2025-01-11 18:29:46 +0200

    small changes

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
+import React, {useState} from 'react';
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}>
+          <View style={styles.container}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>This is modal...</Text>
+              <Pressable
+                  style={styles.pressable}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.buttonText}>Close</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <Pressable
+            style={styles.pressable}
+            onPress={() => setModalVisible(true)}>
+          <Text style={styles.buttonText}>Show modal message</Text>
+        </Pressable>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
@@ -16,5 +42,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  pressable: {
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    fontSize: 20,
   },
 });
